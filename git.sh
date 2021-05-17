93da182238c11ae19fb0377cee3cc40f599eca27
Tom <tom@tdpain.net>
2021-05-17 19:10:40 +0100

    Update git.sh

--- git.sh
+++ git.sh
@@ -6,5 +6,6 @@ add-apt-repository ppa:git-core/ppa
 apt update
 apt install -y git
 
+# Configures global gitignore file - edit ~/.config/global.gitignore and add rules
 touch ~/.config/global.gitignore
 git config --global core.excludesFile '~/.config/global.gitignore'
