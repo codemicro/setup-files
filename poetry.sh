49916dacdcf4bad895ffe52d7a9d44d7b505c75b
AKU <tom@tdpain.net>
2022-01-30 11:48:54 +0000

    Update Poetry script
    Signed-off-by: AKU <tom@tdpain.net>

--- poetry.sh
+++ poetry.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# 2021-06-01 - https://python-poetry.org
+# 2022-01-30 - https://python-poetry.org
 
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
-echo "source \$HOME/.poetry/env" >> $HOME/.bashrc
+# This is deprecated - add --uninstall to remove old versions. curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+curl -sSL https://install.python-poetry.org | python3 -
+# This was used with the deprecated installer. echo "source \$HOME/.poetry/env" >> $HOME/.bashrc
