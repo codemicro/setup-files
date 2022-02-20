59d54e6345db3b1184fbe6bbd8e6430003db0f18
AKU <tom@tdpain.net>
2022-02-20 14:22:55 +0000

    Switch Kitty installation method
    Signed-off-by: AKU <tom@tdpain.net>

--- kitty.sh
+++ kitty.sh
@@ -2,16 +2,17 @@
 
 set -ex
 
-TEMP_DIR="/tmp/kittyInstall"
+# TEMP_DIR="/tmp/kittyInstall"
+#
+# mkdir -p $TEMP_DIR
+#
+# (
+# 	cd $TEMP_DIR
+#	wget https://github.com/kovidgoyal/kitty/releases/download/v0.24.2/kitty-0.24.2-x86_64.txz
+# 	tar xvf kitty-0.24.2-x86_64.txz
+# 	sudo cp bin/kitty /usr/local/bin
+# )
+#
+# rm -rf $TEMP_DIR
 
-mkdir -p $TEMP_DIR
-
-(
-	cd $TEMP_DIR
-
-	wget https://github.com/kovidgoyal/kitty/releases/download/v0.24.2/kitty-0.24.2-x86_64.txz
-	tar xvf kitty-0.24.2-x86_64.txz
-	sudo cp bin/kitty /usr/local/bin
-)
-
-rm -rf $TEMP_DIR
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
