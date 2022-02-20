4f1713fc34570579ea99137f0495ecb0f8ddba12
AKU <tom@tdpain.net>
2022-02-20 14:19:09 +0000

    Add kitty and picom scripts
    Signed-off-by: AKU <tom@tdpain.net>

---
+++ kitty.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+TEMP_DIR="/tmp/kittyInstall"
+
+mkdir -p $TEMP_DIR
+
+(
+	cd $TEMP_DIR
+
+	wget https://github.com/kovidgoyal/kitty/releases/download/v0.24.2/kitty-0.24.2-x86_64.txz
+	tar xvf kitty-0.24.2-x86_64.txz
+	sudo cp bin/kitty /usr/local/bin
+)
+
+rm -rf $TEMP_DIR
