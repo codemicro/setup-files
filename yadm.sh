73a4bf0e0d7a51eaa040eefff19540860731841a
AKP <tom@tdpain.net>
2022-04-13 12:57:13 +0100

    Update yadm.sh
    Signed-off-by: AKP <tom@tdpain.net>

--- yadm.sh
+++ yadm.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-curl -fLo /usr/local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/3.2.1/yadm && chmod a+x /usr/local/bin/yadm
+VERSION="3.2.1"
+
+curl -fLo /usr/local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/$VERSION/yadm
+chmod a+x /usr/local/bin/yadm
